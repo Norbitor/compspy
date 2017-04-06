496a68044a7f76d55d7fcc5b68d04a7ff0e39a9b
Norbert Langner <norbitor@gmail.com>
2017-04-06 13:37:26 +0200

    Experiment of grabbing screenshots and processes

---
+++ csa_experiment/CSATest/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace CSATest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Zrzut";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(746, 451);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(764, 41);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(325, 451);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1101, 504);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ csa_experiment/CSATest/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Management;
+
+namespace CSATest
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap _bmpScreenCapture;
+        public Form1()
+        {
+            InitializeComponent();
+            _bmpScreenCapture = new Bitmap(Screen.PrimaryScreen.Bounds.Width,
+                                            Screen.PrimaryScreen.Bounds.Height);
+
+            listView1.View = View.Details;
+            listView1.Columns.Add("ProcName");
+            listView1.Columns.Add("PID");
+            listView1.Columns.Add("WT");
+            listView1.Columns.Add("US");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (Graphics g = Graphics.FromImage(_bmpScreenCapture))
+            {
+                g.CopyFromScreen(Screen.PrimaryScreen.Bounds.X,
+                                    Screen.PrimaryScreen.Bounds.Y,
+                                    0, 0,
+                                    _bmpScreenCapture.Size,
+                                    CopyPixelOperation.SourceCopy);
+            }
+            pictureBox1.Image = _bmpScreenCapture;
+
+            Process[] procs = Process.GetProcesses();
+
+            var x = from p in procs
+                    
+                    select p;
+
+            listView1.Items.Clear();
+            foreach(var Proc in x)
+            {
+                ListViewItem it = new ListViewItem(Proc.ProcessName);
+                it.SubItems.Add(Proc.Id.ToString());
+                it.SubItems.Add(Proc.MainWindowTitle);
+                it.SubItems.Add(/*GetProcessOwner(Proc.Id)*/ "");
+                listView1.Items.Add(it);
+            }
+            
+        }
+
+        private string GetProcessOwner(int processId)
+        {
+            string query = "Select * From Win32_Process Where ProcessID = " + processId;
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
+            ManagementObjectCollection processList = searcher.Get();
+
+            foreach (ManagementObject obj in processList)
+            {
+                string[] argList = new string[] { string.Empty, string.Empty };
+                int returnVal = Convert.ToInt32(obj.InvokeMethod("GetOwner", argList));
+                if (returnVal == 0)
+                {
+                    // return DOMAIN\user
+                    return argList[1] + "\\" + argList[0];
+                }
+            }
+
+            return "";
+        }
+    }
+}
