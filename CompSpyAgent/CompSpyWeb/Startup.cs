393278958ea4769b5d46c4bb7be63d01c88cfe63
Norbert Langner <norbitor@gmail.com>
2017-05-04 14:23:05 +0200

    Add SignalR library to server part

---
+++ CompSpyAgent/CompSpyWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Owin;
+using Microsoft.Owin;
+[assembly: OwinStartup(typeof(CompSpyWeb.Startup))]
+
+namespace CompSpyWeb
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
