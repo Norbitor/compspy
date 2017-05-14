0adaaa7417c6324f0e452fe9359c243141c6ed9b
Norbert Langner <norbitor@gmail.com>
2017-05-15 00:12:03 +0200

    Create UserController for user management. Initial user list view. Delete demo content

---
+++ CompSpyAgent/CompSpyWeb/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using CompSpyWeb.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CompSpyWeb.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            if (Session["UserID"] == null)
+            {
+                return RedirectToAction("", "Home");
+            }
+            if (CheckUserPermission())
+            {
+                var ctx = new CompSpyContext();
+                return View(ctx.Users.ToList());
+            }
+            return RedirectToAction("", "Home");
+        }
+
+        private bool CheckUserPermission()
+        {
+            int uid = (int)Session["UserID"];
+            using (var ctx = new CompSpyContext())
+            {
+                var us = ctx.Users.Where(u => u.UserID == uid).FirstOrDefault();
+                return us.IsAdmin;
+            }
+        }
+    }
+}
