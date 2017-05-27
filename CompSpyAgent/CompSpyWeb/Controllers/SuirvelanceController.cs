1173ba805647254b6983ff5f33b328449c29fb61
Norbert Langner <norbitor@gmail.com>
2017-05-27 23:36:40 +0200

    Classroom list in Suirvelance controller

--- CompSpyAgent/CompSpyWeb/Controllers/SuirvelanceController.cs
+++ CompSpyAgent/CompSpyWeb/Controllers/SuirvelanceController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CompSpyWeb.DAL;
+using CompSpyWeb.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,30 @@ namespace CompSpyWeb.Controllers
 {
     public class SuirvelanceController : Controller
     {
+        private CompSpyContext db = new CompSpyContext();
+
         // GET: Suirvelance
         public ActionResult Index()
         {
-            return View();
+            if (Session["UserID"] == null)
+            {
+                return RedirectToAction("", "Home");
+            }
+            ICollection<Classroom> classrooms;
+            if (CheckUserPermission())
+            {
+                classrooms = db.Classrooms.ToList();
+            }
+            else
+            {
+                int uid = (int)Session["UserID"];
+                classrooms = (from c in db.Classrooms
+                             join cp in db.ClassroomPermissions on c.ID equals cp.ClassroomID
+                             where cp.UserID == uid
+                             select c).ToList();
+            }
+
+            return View(classrooms);
         }
 
         // GET: Suirvelance/ShowRoom/4
@@ -24,6 +46,22 @@ namespace CompSpyWeb.Controllers
         public ActionResult ShowComputer(int id)
         {
             return View();
+        }
+
+        private bool CheckUserPermission()
+        {
+            int uid = (int)Session["UserID"];
+            var us = db.Users.Where(u => u.UserID == uid).FirstOrDefault();
+            return us.IsAdmin;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
